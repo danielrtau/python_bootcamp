dbc5468f7d86a15d3d9e4dec27bed147a32bce78
Daniel R. T <daniel.rodriguez1990@hotmail.com>
2023-02-22 17:40:18 +0100

    ex07 updated

--- ex07/filterwords.py
+++ ex07/filterwords.py
@@ -11,6 +11,10 @@ cadena2 = cadena.split(" ")
 cadena3 = []
 for i in cadena2:
     if len(i) >= number:
-        cadena3.append(i)
+        if i.find(',') != -1 or i.find('.') != -1:
+            y = i[0:-1]
+            cadena3.append(y)
+        else:
+            cadena3.append(i)
 print(cadena3)
 
